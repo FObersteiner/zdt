b30fc84d979162c320042ee05fe87b1e2db43085
FObersteiner <f.obersteiner@posteo.de>
2024-09-12 07:48:23 +0200

    update docs / changelog, prepare patch 3

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ const std = @import("std");
 const builtin = @import("builtin");
 const log = std.log.scoped(.zdt_build);
 
-const zdt_version = std.SemanticVersion{ .major = 0, .minor = 2, .patch = 2 };
+const zdt_version = std.SemanticVersion{ .major = 0, .minor = 2, .patch = 3 };
 
 const example_files = [_][]const u8{
     "ex_demo",
