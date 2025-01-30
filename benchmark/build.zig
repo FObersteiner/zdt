9a8912186cbc0ba4512a97fb275022e2f4180838
FObersteiner <f.obersteiner@posteo.de>
2025-01-30 20:42:08 +0100

    more benchmarks

--- benchmark/build.zig
+++ benchmark/build.zig
@@ -21,12 +21,18 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const zdt_030 = b.dependency("zdt_030", .{});
+    const zbench = b.dependency("zbench", .{});
+    const zeit = b.dependency("zeit", .{});
+    const zdt_023 = b.dependency("zdt_023", .{});
     const zdt_045 = b.dependency("zdt_045", .{});
+    const zdt_046 = b.dependency("zdt_046", .{});
     // TODO : labeled switch parser will require zig 0.14
 
-    const zdt_030_module = zdt_030.module("zdt");
+    const zbench_module = zbench.module("zbench");
+    const zeit_module = zeit.module("zeit");
+    const zdt_023_module = zdt_023.module("zdt");
     const zdt_045_module = zdt_045.module("zdt");
+    const zdt_046_module = zdt_046.module("zdt");
 
     for (benchmarks) |benchname| {
         const _bench = b.addExecutable(.{
@@ -36,8 +42,11 @@ pub fn build(b: *std.Build) void {
             .optimize = optimize,
         });
         b.installArtifact(_bench);
-        _bench.root_module.addImport("zdt_030", zdt_030_module);
+        _bench.root_module.addImport("zbench", zbench_module);
+        _bench.root_module.addImport("zeit", zeit_module);
+        _bench.root_module.addImport("zdt_023", zdt_023_module);
         _bench.root_module.addImport("zdt_045", zdt_045_module);
+        _bench.root_module.addImport("zdt_046", zdt_046_module);
         _bench.linkLibC();
 
         const run_cmd = b.addRunArtifact(_bench);
