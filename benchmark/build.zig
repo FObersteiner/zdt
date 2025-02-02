e3cddee814ca300b1ad8d7012a7bc3e70848a8b1
FObersteiner <f.obersteiner@posteo.de>
2025-02-02 18:49:22 +0100

    adjust benchmark

--- benchmark/build.zig
+++ benchmark/build.zig
@@ -6,13 +6,13 @@ const benchmarks = [_][]const u8{
     "main",
 };
 
-const req_zig_version = "0.13.0";
+const vers_zig13 = std.SemanticVersion.parse("0.13.0") catch unreachable;
+
 comptime {
-    const req_zig = std.SemanticVersion.parse(req_zig_version) catch unreachable;
-    if (builtin.zig_version.order(req_zig) == .lt) {
+    if (builtin.zig_version.order(vers_zig13) == .lt) {
         @compileError(std.fmt.comptimePrint(
             "Your Zig version v{} does not meet the minimum build requirement of v{}",
-            .{ builtin.zig_version, req_zig },
+            .{ builtin.zig_version, vers_zig13 },
         ));
     }
 }
@@ -21,16 +21,16 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const zbench = b.dependency("zbench", .{});
+    // const zbench = b.dependency("zbench", .{});
     const zeit = b.dependency("zeit", .{});
-    const zdt_023 = b.dependency("zdt_023", .{});
+    // const zdt_023 = b.dependency("zdt_023", .{});
     const zdt_045 = b.dependency("zdt_045", .{});
     const zdt_current = b.dependency("zdt_current", .{});
     // TODO : labeled switch parser will require zig 0.14
 
-    const zbench_module = zbench.module("zbench");
+    //    const zbench_module = zbench.module("zbench");
     const zeit_module = zeit.module("zeit");
-    const zdt_023_module = zdt_023.module("zdt");
+    // const zdt_023_module = zdt_023.module("zdt");
     const zdt_045_module = zdt_045.module("zdt");
     const zdt_current_module = zdt_current.module("zdt");
 
@@ -42,9 +42,9 @@ pub fn build(b: *std.Build) void {
             .optimize = optimize,
         });
         b.installArtifact(_bench);
-        _bench.root_module.addImport("zbench", zbench_module);
+        //        _bench.root_module.addImport("zbench", zbench_module);
         _bench.root_module.addImport("zeit", zeit_module);
-        _bench.root_module.addImport("zdt_023", zdt_023_module);
+        // _bench.root_module.addImport("zdt_023", zdt_023_module);
         _bench.root_module.addImport("zdt_045", zdt_045_module);
         _bench.root_module.addImport("zdt_current", zdt_current_module);
         _bench.linkLibC();
