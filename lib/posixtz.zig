f9af0ee0d0ce84b3d81d8ae8d867c52acfaa01bb
FObersteiner <f.obersteiner@posteo.de>
2025-03-07 20:19:23 +0100

    minor tweaks

--- lib/posixtz.zig
+++ lib/posixtz.zig
@@ -4,29 +4,31 @@ const std = @import("std");
 const assert = std.debug.assert;
 const cal = @import("calendar.zig");
 const UTCoffset = @import("./UTCoffset.zig");
+const TzError = @import("./errors.zig").TzError;
 
-const log = std.log.scoped(.test_posixtz);
+/// The default DST transition time is 2 am local time
+const default_transition_time: i32 = 2 * std.time.s_per_hour;
 
 /// Time zone rules from POSIX TZ string
-pub const Tz = struct {
-    std_designation: []const u8,
+pub const PosixTz = struct {
     std_offset: i32,
+    std_designation: []const u8, // safe to use []const u8 / pointers here since the basis also is []const u8
+    dst_offset: ?i32 = null,
     dst_designation: ?[]const u8 = null,
-    dst_offset: i32 = 0,
     dst_range: ?struct { start: Rule, end: Rule } = null,
 
     pub const Rule = union(enum) {
         JulianDay: struct {
             /// 1 <= day <= 365. Leap days are not counted and are impossible to refer to
             day: u16,
-            /// The default DST transition time is 02:00:00 local time
-            time: i32 = 2 * std.time.s_per_hour,
+            /// Transition time
+            time: i32 = default_transition_time,
         },
         JulianDayZero: struct {
             /// 0 <= day <= 365. Leap days are counted, and can be referred to.
             day: u16,
-            /// The default DST transition time is 02:00:00 local time
-            time: i32 = 2 * std.time.s_per_hour,
+            /// Transition time
+            time: i32 = default_transition_time,
         },
         /// In the format of "Mm.n.d", where m = month, n = n, and d = day.
         MonthNthWeekDay: struct {
@@ -46,8 +48,8 @@ pub const Tz = struct {
             n: u8,
             /// Day of the week. 0 <= day <= 6. Day zero is Sunday.
             day: u8,
-            /// The default DST transition time is 02:00:00 local time
-            time: i32 = 2 * std.time.s_per_hour,
+            /// Transition time
+            time: i32 = default_transition_time,
         },
 
         pub fn isAtStartOfYear(rule: Rule) bool {
@@ -60,9 +62,8 @@ pub const Tz = struct {
 
         pub fn isAtEndOfYear(rule: Rule) bool {
             switch (rule) {
-                // TODO : this only seems to make sense for JulianDay rule; otherwise error?
                 .JulianDay => |j| return j.day == 365 and j.time >= 24,
-                // Since JulianDayZero dates account for leap year, it would vary depending on the year.
+                // Since JulianDayZero dates account for leap years, it would vary depending on the year.
                 .JulianDayZero => return false,
                 // There is also no way to specify "end of the year" with MonthNthWeekDay rules
                 .MonthNthWeekDay => return false,
@@ -71,150 +72,98 @@ pub const Tz = struct {
 
         /// Returned value is the Unix time of the transition in given year,
         /// including the local UTC offset (!)
-        pub fn toSecs(rule: Rule, year: u16) i64 {
+        pub fn toUnixTimeLocal(rule: Rule, year: u16) i64 {
             const is_leap: bool = cal.isLeapYear(year);
             const start_of_year = cal.dateToRD([3]u16{ year, 1, 1 });
-
             var t = @as(i64, start_of_year) * std.time.s_per_day;
 
             switch (rule) {
                 .JulianDay => |j| {
                     var x: i64 = j.day;
                     if (x < 60 or !is_leap) x -= 1;
-                    t += std.time.s_per_day * x;
-                    t += j.time;
+                    t += std.time.s_per_day * x + j.time;
                 },
                 .JulianDayZero => |j| {
-                    t += std.time.s_per_day * @as(i64, j.day);
-                    t += j.time;
+                    t += std.time.s_per_day * @as(i64, j.day) + j.time;
                 },
                 .MonthNthWeekDay => |mwd| {
-                    const DAYS_PER_WEEK = 7;
-
                     const days_since_epoch: i32 = cal.dateToRD([3]u16{ year, mwd.month, 1 });
-
                     const first_weekday_of_month = cal.weekdayFromUnixdays(days_since_epoch);
-
                     const weekday_offset_for_month = if (first_weekday_of_month <= mwd.day)
                         // the first matching weekday is during the first week of the month
                         mwd.day - first_weekday_of_month
                     else
                         // the first matching weekday is during the second week of the month
-                        mwd.day + DAYS_PER_WEEK - first_weekday_of_month;
+                        mwd.day + 7 - first_weekday_of_month;
 
                     const days_since_start_of_month = switch (mwd.n) {
-                        1...4 => |n| (n - 1) * DAYS_PER_WEEK + weekday_offset_for_month,
-                        5 => if (weekday_offset_for_month + (4 * DAYS_PER_WEEK) >= cal.daysInMonth(mwd.month, is_leap))
+                        1...4 => |n| (n - 1) * 7 + weekday_offset_for_month,
+                        5 => if (weekday_offset_for_month + 28 >= cal.daysInMonth(mwd.month, is_leap))
                             // the last matching weekday is during the 4th week of the month
-                            (4 - 1) * DAYS_PER_WEEK + weekday_offset_for_month
+                            21 + weekday_offset_for_month
                         else
                             // the last matching weekday is during the 5th week of the month
-                            (5 - 1) * DAYS_PER_WEEK + weekday_offset_for_month,
+                            28 + weekday_offset_for_month,
                         else => unreachable,
                     };
 
-                    t += (days_since_epoch - start_of_year) * std.time.s_per_day + std.time.s_per_day * @as(i64, days_since_start_of_month);
-                    t += mwd.time;
+                    t += (days_since_epoch - start_of_year) * std.time.s_per_day +
+                        std.time.s_per_day * @as(i64, days_since_start_of_month) +
+                        mwd.time;
                 },
             }
             return t;
         }
-
-        pub fn format(
-            this: @This(),
-            comptime fmt: []const u8,
-            options: std.fmt.FormatOptions,
-            writer: anytype,
-        ) !void {
-            _ = fmt;
-            _ = options;
-
-            switch (this) {
-                .JulianDay => |julian_day| {
-                    try std.fmt.format(writer, "J{}", .{julian_day.day});
-                },
-                .JulianDayZero => |julian_day_zero| {
-                    try std.fmt.format(writer, "{}", .{julian_day_zero.day});
-                },
-                .MonthNthWeekDay => |month_week_day| {
-                    try std.fmt.format(writer, "M{}.{}.{}", .{
-                        month_week_day.month,
-                        month_week_day.n,
-                        month_week_day.day,
-                    });
-                },
-            }
-
-            const time = switch (this) {
-                inline else => |rule| rule.time,
-            };
-
-            // Only write out the time if it is not the default time of 02:00
-            if (time != 2 * std.time.s_per_hour) {
-                const seconds = @mod(time, std.time.s_per_min);
-                const minutes = @mod(@divTrunc(time, std.time.s_per_min), 60);
-                const hours = @divTrunc(@divTrunc(time, std.time.s_per_min), 60);
-
-                try std.fmt.format(writer, "/{}", .{hours});
-                if (minutes != 0 or seconds != 0) {
-                    try std.fmt.format(writer, ":{}", .{minutes});
-                }
-                if (seconds != 0) {
-                    try std.fmt.format(writer, ":{}", .{seconds});
-                }
-            }
-        }
     };
 
     /// Get the offset from UTC at given Unix time, including Daylight Saving Time
-    // TODO : consider propagating error instead of using 'unreachable'
-    pub fn utcOffsetAt(tz: *const Tz, unix_seconds: i64) UTCoffset {
+    pub fn utcOffsetAt(tz: *const PosixTz, unix_seconds: i64) TzError!UTCoffset {
         const dst_designation = tz.dst_designation orelse {
+            assert(tz.dst_offset == null);
             assert(tz.dst_range == null);
-            return UTCoffset.fromSeconds(tz.std_offset, tz.std_designation, false) catch unreachable;
+            return try UTCoffset.fromSeconds(tz.std_offset, tz.std_designation, false);
         };
         if (tz.dst_range) |range| {
             const ymd = cal.rdToDate(@truncate(@divFloor(unix_seconds, std.time.s_per_day)));
             const year = ymd[0];
 
-            const start_dst = range.start.toSecs(year) - tz.std_offset;
-            const end_dst = range.end.toSecs(year) - tz.dst_offset;
+            const start_dst = range.start.toUnixTimeLocal(year) - tz.std_offset;
+            const end_dst = range.end.toUnixTimeLocal(year) - tz.dst_offset.?;
             const is_dst_all_year = range.start.isAtStartOfYear() and range.end.isAtEndOfYear();
 
             if (is_dst_all_year) {
-                return UTCoffset.fromSeconds(tz.dst_offset, dst_designation, true) catch unreachable;
+                return try UTCoffset.fromSeconds(tz.dst_offset.?, dst_designation, true);
             }
 
             if (start_dst < end_dst) {
                 if (unix_seconds >= start_dst and unix_seconds < end_dst) {
-                    return UTCoffset.fromSeconds(tz.dst_offset, dst_designation, true) catch unreachable;
+                    return try UTCoffset.fromSeconds(tz.dst_offset.?, dst_designation, true);
                 } else {
-                    return UTCoffset.fromSeconds(tz.std_offset, tz.std_designation, false) catch unreachable;
+                    return try UTCoffset.fromSeconds(tz.std_offset, tz.std_designation, false);
                 }
             } else {
                 if (unix_seconds >= end_dst and unix_seconds < start_dst) {
-                    return UTCoffset.fromSeconds(tz.std_offset, tz.std_designation, false) catch unreachable;
+                    return try UTCoffset.fromSeconds(tz.std_offset, tz.std_designation, false);
                 } else {
-                    return UTCoffset.fromSeconds(tz.dst_offset, dst_designation, true) catch unreachable;
+                    return try UTCoffset.fromSeconds(tz.dst_offset.?, dst_designation, true);
                 }
             }
-        } else {
-            return UTCoffset.fromSeconds(tz.std_offset, tz.std_designation, false) catch unreachable;
         }
+        return try UTCoffset.fromSeconds(tz.std_offset, tz.std_designation, false);
     }
 };
 
-pub fn parsePosixTzString(string: []const u8) !Tz {
-    var result = Tz{ .std_designation = undefined, .std_offset = undefined };
+/// Parse a POSIX TZ string like 'PST8PDT,M3.2.0,M11.1.0' to a set of rules and
+/// offsets from UTC.
+pub fn parsePosixTzString(string: []const u8) !PosixTz {
+    var result = PosixTz{ .std_designation = undefined, .std_offset = undefined };
     var idx: usize = 0;
 
     result.std_designation = try parseDesignation(string, &idx);
 
     // multiply by -1 to get offset as seconds East of Greenwich as TZif specifies it:
     result.std_offset = try parseHHmmss(string[idx..], &idx) * -1;
-    if (idx >= string.len) {
-        return result;
-    }
+    if (idx >= string.len) return result;
 
     if (string[idx] != ',') {
         result.dst_designation = try parseDesignation(string, &idx);
@@ -226,9 +175,7 @@ pub fn parsePosixTzString(string: []const u8) !Tz {
             result.dst_offset = result.std_offset + std.time.s_per_hour;
         }
 
-        if (idx >= string.len) {
-            return result;
-        }
+        if (idx >= string.len) return result;
     }
 
     assert(string[idx] == ',');
@@ -247,6 +194,7 @@ pub fn parsePosixTzString(string: []const u8) !Tz {
     return result;
 }
 
+/// Parse a POSIX TZ designation such as 'PST' in 'PST8PDT,M3.2.0,M11.1.0'
 fn parseDesignation(string: []const u8, idx: *usize) ![]const u8 {
     const quoted = string[idx.*] == '<';
     if (quoted) idx.* += 1;
@@ -267,73 +215,52 @@ fn parseDesignation(string: []const u8, idx: *usize) ![]const u8 {
     return error.InvalidFormat;
 }
 
-fn parseRule(_string: []const u8) !Tz.Rule {
+/// Parse a POSIX TZ rule such as 'M3.2.0' in 'PST8PDT,M3.2.0,M11.1.0'
+/// to machine-readable representation of 'DST starts second Sunday in March'
+fn parseRule(_string: []const u8) !PosixTz.Rule {
     var string = _string;
     if (string.len < 2) return error.InvalidFormat;
 
     const time: i32 = if (std.mem.indexOf(u8, string, "/")) |start_of_time| parse_time: {
         const time_string = string[start_of_time + 1 ..];
-
         var i: usize = 0;
         const time = try parseHHmmss(time_string, &i);
-
         // The time at the end of the rule should be the last thing in the string. Fixes the parsing to return
         // an error in cases like "/2/3", where they have some extra characters.
-        if (i != time_string.len) {
-            return error.InvalidFormat;
-        }
-
+        if (i != time_string.len) return error.InvalidFormat;
         string = string[0..start_of_time];
-
         break :parse_time time;
-    } else 2 * std.time.s_per_hour;
+    } else default_transition_time;
 
-    if (string[0] == 'J') {
-        const julian_day1 = std.fmt.parseInt(u16, string[1..], 10) catch |err| switch (err) {
-            error.InvalidCharacter => return error.InvalidFormat,
-            error.Overflow => return error.InvalidFormat,
-        };
-
-        if (julian_day1 < 1 or julian_day1 > 365) return error.InvalidFormat;
-        return Tz.Rule{ .JulianDay = .{ .day = julian_day1, .time = time } };
-    } else if (std.ascii.isDigit(string[0])) {
-        const julian_day0 = std.fmt.parseInt(u16, string[0..], 10) catch |err| switch (err) {
-            error.InvalidCharacter => return error.InvalidFormat,
-            error.Overflow => return error.InvalidFormat,
-        };
-
-        if (julian_day0 > 365) return error.InvalidFormat;
-        return Tz.Rule{ .JulianDayZero = .{ .day = julian_day0, .time = time } };
-    } else if (string[0] == 'M') {
-        var split_iter = std.mem.splitScalar(u8, string[1..], '.');
-        const m_str = split_iter.next() orelse return error.InvalidFormat;
-        const n_str = split_iter.next() orelse return error.InvalidFormat;
-        const d_str = split_iter.next() orelse return error.InvalidFormat;
-
-        const m = std.fmt.parseInt(u8, m_str, 10) catch |err| switch (err) {
-            error.InvalidCharacter => return error.InvalidFormat,
-            error.Overflow => return error.InvalidFormat,
-        };
-        const n = std.fmt.parseInt(u8, n_str, 10) catch |err| switch (err) {
-            error.InvalidCharacter => return error.InvalidFormat,
-            error.Overflow => return error.InvalidFormat,
-        };
-        const d = std.fmt.parseInt(u8, d_str, 10) catch |err| switch (err) {
-            error.InvalidCharacter => return error.InvalidFormat,
-            error.Overflow => return error.InvalidFormat,
-        };
-
-        if (m < 1 or m > 12) return error.InvalidFormat;
-        if (n < 1 or n > 5) return error.InvalidFormat;
-        if (d > 6) return error.InvalidFormat;
-
-        return Tz.Rule{ .MonthNthWeekDay = .{ .month = m, .n = n, .day = d, .time = time } };
-    } else {
-        return error.InvalidFormat;
+    switch (string[0]) {
+        'J' => {
+            const julian_day1 = std.fmt.parseInt(u16, string[1..], 10) catch return error.InvalidFormat;
+            if (julian_day1 < 1 or julian_day1 > 365) return error.InvalidFormat;
+            return PosixTz.Rule{ .JulianDay = .{ .day = julian_day1, .time = time } };
+        },
+        '0'...'9' => {
+            const julian_day0 = std.fmt.parseInt(u16, string[0..], 10) catch return error.InvalidFormat;
+            if (julian_day0 > 365) return error.InvalidFormat;
+            return PosixTz.Rule{ .JulianDayZero = .{ .day = julian_day0, .time = time } };
+        },
+        'M' => {
+            var split_iter = std.mem.splitScalar(u8, string[1..], '.');
+            const m_str = split_iter.next() orelse return error.InvalidFormat;
+            const n_str = split_iter.next() orelse return error.InvalidFormat;
+            const d_str = split_iter.next() orelse return error.InvalidFormat;
+            const m = std.fmt.parseInt(u8, m_str, 10) catch return error.InvalidFormat;
+            const n = std.fmt.parseInt(u8, n_str, 10) catch return error.InvalidFormat;
+            const d = std.fmt.parseInt(u8, d_str, 10) catch return error.InvalidFormat;
+            if (m < 1 or m > 12) return error.InvalidFormat;
+            if (n < 1 or n > 5) return error.InvalidFormat;
+            if (d > 6) return error.InvalidFormat;
+            return PosixTz.Rule{ .MonthNthWeekDay = .{ .month = m, .n = n, .day = d, .time = time } };
+        },
+        else => return error.InvalidFormat,
     }
 }
 
-/// Parses hh[:mm[:ss]] to a number of seconds. Hours may be one digit long. Minutes and seconds must be two digits.
+/// Parses hh[:mm[:ss]] to number of seconds. Hours may be one digit long. Minutes and seconds must be two digits.
 fn parseHHmmss(string: []const u8, idx_ptr: *usize) !i32 {
     var _string = string;
     var sign: i2 = 1;
@@ -358,13 +285,8 @@ fn parseHHmmss(string: []const u8, idx_ptr: *usize) !i32 {
 
     var segment_iter = std.mem.splitScalar(u8, _string, ':');
     const hour_string = segment_iter.next() orelse return error.EmptyString;
-    const hours = std.fmt.parseInt(u32, hour_string, 10) catch |err| switch (err) {
-        error.InvalidCharacter => return error.InvalidFormat,
-        error.Overflow => return error.InvalidFormat,
-    };
-    if (hours > 167) {
-        return error.InvalidFormat;
-    }
+    const hours = std.fmt.parseInt(u32, hour_string, 10) catch return error.InvalidFormat;
+    if (hours > 167) return error.InvalidFormat;
     result += std.time.s_per_hour * @as(i32, @intCast(hours));
 
     if (segment_iter.next()) |minute_string| {
