ce5f4ad52e64a173d0af38930cc8febb6e8852be
FObersteiner <f.obersteiner@posteo.de>
2025-03-08 12:20:57 +0100

    update example

--- examples/ex_offsetTz-posixTz.zig
+++ examples/ex_offsetTz-posixTz.zig
@@ -3,20 +3,28 @@ const builtin = @import("builtin");
 
 const zdt = @import("zdt");
 const Datetime = zdt.Datetime;
+const Timezone = zdt.Timezone;
 const UTCoffset = zdt.UTCoffset;
 
 pub fn main() !void {
     println("---> UTC offset example", .{});
 
-    const offset = try UTCoffset.fromSeconds(3600, "UTC+1");
-    var a_date = try Datetime.fromFields(.{ .year = 1970, .tz_options = .{ .utc_offset = offset } });
+    const offset = try UTCoffset.fromSeconds(3600, "UTC+1", false);
+    var a_date = try Datetime.fromFields(.{ .year = 1970, .hour = 1, .tz_options = .{ .utc_offset = offset } });
     println("datetime: {s}", .{a_date});
     println("offset name: {s}", .{a_date.tzAbbreviation()});
 
-    const other_offset = try UTCoffset.fromSeconds(-5 * 3600, "UTC-5");
+    const other_offset = try UTCoffset.fromSeconds(-5 * 3600, "UTC-5", false);
     var a_date_other_tz = try a_date.tzConvert(.{ .utc_offset = other_offset });
     println("datetime in other tz: {s}", .{a_date_other_tz});
     println("other offset name: {s}", .{a_date_other_tz.tzAbbreviation()});
+
+    println("\n---> POSIX TZ example", .{});
+    const posixTz = try Timezone.fromPosixTz("GMT0BST-1,M3.5.0/1:00,M10.5.0/2:00");
+    var a_date_posix_tz = try a_date.tzConvert(.{ .tz = &posixTz });
+    println("datetime in posix tz: {s}", .{a_date_posix_tz});
+    println("posix tz name: {s}", .{a_date_posix_tz.tzName()});
+    println("posix tz offset name: {s}", .{a_date_posix_tz.tzAbbreviation()});
 }
 
 fn println(comptime fmt: []const u8, args: anytype) void {
