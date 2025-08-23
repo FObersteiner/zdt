421f8206bcfaaa614a1f7995cf4456915e0398eb
FObersteiner <f.obersteiner@posteo.de>
2025-08-23 15:05:44 +0200

    revise benchmarks

--- benchmark/build.zig
+++ benchmark/build.zig
@@ -20,15 +20,11 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // const zeit = b.dependency("zeit", .{});
-    // const zdt_023 = b.dependency("zdt_023", .{});
-    // const zdt_045 = b.dependency("zdt_045", .{});
+    const zeit = b.dependency("zeit", .{});
     const zbench = b.dependency("zbench", .{});
     const zdt_latest = b.dependency("zdt_latest", .{});
 
-    // const zeit_module = zeit.module("zeit");
-    // const zdt_023_module = zdt_023.module("zdt");
-    // const zdt_045_module = zdt_045.module("zdt");
+    const zeit_module = zeit.module("zeit");
     const zbench_module = zbench.module("zbench");
     const zdt_latest_module = zdt_latest.module("zdt");
 
@@ -42,9 +38,7 @@ pub fn build(b: *std.Build) void {
             }),
         });
         b.installArtifact(_bench);
-        // _bench.root_module.addImport("zeit", zeit_module);
-        // _bench.root_module.addImport("zdt_023", zdt_023_module);
-        // _bench.root_module.addImport("zdt_045", zdt_045_module);
+        _bench.root_module.addImport("zeit", zeit_module);
         _bench.root_module.addImport("zbench", zbench_module);
         _bench.root_module.addImport("zdt_current", zdt_latest_module);
         _bench.linkLibC();
